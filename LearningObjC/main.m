4b7741021d9409b5c5a6d9ab8e8a943929310a47
Igor <klyuzhevigor>
2021-01-16 23:23:05 +0300

    Initial Commit

---
+++ LearningObjC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LearningObjC
+//
+//  Created by Игорь Клюжев on 16.01.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
